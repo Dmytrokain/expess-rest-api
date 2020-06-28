b8a0812640e093c09a745d2dc124965894103b34
dmytrokain <dimikowowik@gmail.com>
2020-06-28 17:23:26 +0300

    Create endpoints for GET, PATCH and Delete the subject.

--- src/controllers/subjectsController.js
+++ src/controllers/subjectsController.js
@@ -4,12 +4,35 @@ exports.subjectsList = (req, res) => {
   Subject.findAll().then((user) => res.json(user));
 };
 
+exports.getSubject = (req, res) => {
+  Subject.findOne({
+    where: { id: req.params.id },
+  }).then((user) => res.json(user));
+};
+
+exports.deleteSubject = (req, res) => {
+  Subject.destroy({
+    where: { id: req.params.id },
+  }).then((result) => {
+    if (result === 1) {
+      res.status(200).json({ message: 'Subject was deleted' });
+    } else {
+      res.status(404).json({ message: 'Error occured while deleting subject' });
+    }
+  });
+};
+
 exports.createSubject = (req, res) => {
   Subject.create(req.body)
     .then((subject) => res.json(subject))
     .catch((err) => {
-      const errors = err.errors.map((error) => error.message);
       res.status(400);
-      res.json({ errors });
+      res.json({ err });
     });
+};
+
+exports.updateSubject = (req, res) => {
+  Subject.update(req.body, {
+    where: { id: req.params.id },
+  }).then((user) => res.json(user));
 };
